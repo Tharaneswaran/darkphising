664c007bac9d1b549347f821f37a8a8f934081dc
Tharaneswaran <84565857+Tharaneswaran@users.noreply.github.com>
2022-11-17 11:55:49 +0530

    Delete darkphish-frontend directory

--- darkphish-frontend/vue.config.js
+++
@@ -1,9 +0,0 @@
-const { defineConfig } = require('@vue/cli-service')
-module.exports = defineConfig({
-  transpileDependencies: [
-    'vuetify'
-  ],
-  devServer : {
-    proxy : 'http://127.0.0.1:5000/'
-  }
-})
